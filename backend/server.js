884218e910a2ff505cfc1ae3ac1f4b9787e436fe
RansikaChandupa <ransikachandupa@gmail.com>
2025-05-14 13:39:27 +0530

    Test basic API

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,13 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv").config()
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req, res)=>{
+    // console.log("suceess or fail")
+    res.json({message:"Hello"})
+})
+app.listen(PORT, (err)=>{
+    console.log(`App is listening on ${PORT}`);
+})
